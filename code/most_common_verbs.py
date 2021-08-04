860c753a757ceaee718cccb2c6e1b325315b3875
Fcabla <fer_nando_cb@hotmail.com>
2021-08-04 11:50:37 +0200

    json statistics of the most common verbs and prepositions, will be used to build the lookup table

--- code/most_common_verbs.py
+++ code/most_common_verbs.py
@@ -1,12 +1,12 @@
 import spacy
 import pandas as pd
 from spacy.symbols import nsubj, VERB, AUX, PUNCT
-from collections import Counter
+from collections import Counter, OrderedDict
 import re
-import pickle
+import json
 
 INPUT_FILE = "datasets/long-abstracts-sample.csv"
-OUTPUT_FILE = "results/complex_sentences_common_verbs2.pkl"
+OUTPUT_FILE = "results/stats/complex_sentences_common_prep_test.json"
 
 def clean_text(text):
     #remove all the parentheses
@@ -66,6 +66,35 @@ def get_lemmatized_verbs(sentence):
                 verbs.append(token.lemma_)
     return verbs
 
+def get_lemmatized_verbs_prepositions(sentence):
+    """ Returns the lemma of the verbs in the sentence """
+    results = []
+    verbs = []
+    for token in sentence:
+        if token.pos == VERB:
+            verbs.append(token)
+        if token.pos == AUX:
+            if token.head.pos != VERB or token.dep_ not in ["aux","auxpass"]:
+                verbs.append(token)
+    
+    for verb in verbs:
+        children = [x for x in verb.children]
+        prep_found = False
+        for child in children:
+            if child.dep_ == "prep":
+                results.append(f"{verb.lemma_} {child.text}")
+                prep_found = True
+        if not prep_found:
+            results.append(verb.lemma_)
+    return results
+
+def get_prepositions(sentence):
+    preps = []
+    for token in sentence:
+        if token.dep_ == "prep" and token.head.pos in [AUX, VERB]:
+            preps.append(token.text)
+    return preps
+
 def main():
     nlp = spacy.load("en_core_web_trf")
     df = pd.read_csv(INPUT_FILE)
@@ -77,13 +106,16 @@ def main():
         sentences = get_sentences(doc)
         for sentence in sentences:
             #if get_num_verbs(sentence) == 1:
-            verbs.extend(get_lemmatized_verbs(sentence))
-        verb_counter = Counter(verbs)
+            #verbs.extend(get_lemmatized_verbs(sentence))
+            #verbs.extend(get_lemmatized_verbs_prepositions(sentence))
+            verbs.extend(get_prepositions(sentence))
+    verb_counter = Counter(verbs)
+    verb_counter = OrderedDict(verb_counter.most_common())
     print(verb_counter)
 
     # save the results
-    save_file = open(OUTPUT_FILE, "wb")
-    pickle.dump(verb_counter, save_file)
+    save_file = open(OUTPUT_FILE, "w")
+    json.dump(verb_counter, save_file, indent=4)
     save_file.close()
 
 if __name__ == "__main__":
---
+++ docs/coding-week7.md
@@ -0,0 +1,1 @@
+Maybe coding week 7 and 8 together with the creation of the lookuptable and the mechanisms to access and replace.
